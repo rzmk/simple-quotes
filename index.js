309b2a4f4aef5608230303586cb217ff229d1aa8
rzmk <mueez25@gmail.com>
2022-06-14 23:52:33 -0400

    :wrench: Removed null/unknown author quotes

--- index.js
+++ index.js
@@ -4,30 +4,31 @@ let main_text = document.getElementById("click-text");
 
 // Random quote generator and display on page
 function newInspirationalQuote() {
-    fetch('https://type.fit/api/quotes')
-    .then(response => {
+  fetch("https://type.fit/api/quotes")
+    .then((response) => {
       if (response.ok) {
         data = response.json();
         return data; // return json data to next .then
       } else {
-        console.log("FAILURE")
+        console.log("FAILURE");
       }
     })
     .then(function quoteFunction(data) {
       let data_set = data[Math.round(data.length * Math.random())];
-      let quote = data_set.text
-      let author = data_set.author
-      while (quote.length > 50) {
+      let quote = data_set.text;
+      let author = data_set.author;
+      while (quote.length > 50 || author == null) {
         data_set = data[Math.round(data.length * Math.random())];
-        quote = data_set.text
-        author = data_set.author
+        quote = data_set.text;
+        author = data_set.author;
       }
-      document.getElementById('inspirational').onclick = function inspirationalClicked() {
-          document.getElementById('click-text').innerText = `"${quote}"`;
-          document.getElementById('author-text').innerText = `- ${author}`;
+      document.getElementById("inspirational").onclick =
+        function inspirationalClicked() {
+          document.getElementById("click-text").innerText = `"${quote}"`;
+          document.getElementById("author-text").innerText = `- ${author}`;
           quoteFunction(data);
-      };
-    })
+        };
+    });
 }
 
 newInspirationalQuote();
